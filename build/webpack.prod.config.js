df0ae87b856446e290e2aae04c6e4c62f1dce67a
Kirn <wangsguang@gmail.com>
2017-10-28 15:10:27 +0800

    修改编辑方式

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -5,6 +5,8 @@ const base = require('./webpack.base.config');
 module.exports = Object.assign({}, base, {
     output : Object.assign({}, base.output, {
         filename : 'bundle.app.[hash].js',
-        chunkFilename : 'chunk.[id].[hash].js'
-    })
+        chunkFilename : 'chunk.[id].[hash].js',
+        libraryTarget: 'commonjs2'
+    }),
+    externals: Object.keys(require('../package.json').dependencies),
 });
